7bd233acb4ae07cbe48517a3414583cb6f3e96f6
FUNZHOU <130201517+Funzhou000@users.noreply.github.com>
2024-11-07 17:01:14 +0800

    Add files via upload

---
+++ 翁恺c语言/FACTOR阶乘.cpp
@@ -0,0 +1,24 @@
+//#define  _CRT_SECURE_NO_WARNINGS
+//#include <iostream> // 标准输入输出流
+//#include <vector>   // 标准模板库中的向量
+//#include <string>   // C++ 字符串类
+//#include <cmath>    // 数学函数
+//#include<time.h>
+//#include<stdlib.h>
+//int main (){
+//	int n;
+//	scanf("%d", &n);
+//	int fact = 1;
+//	//for (int i = 1; i <=n;i++)
+//	//{
+//	//	fact = i * fact;
+//	//}//正序
+//	for (; n > 1; n--) {
+//		fact *= n;
+//	}//倒序，for可以省略一/2个元素
+//
+//
+//
+//	printf("n!=%d", fact);
+//	return 0;
+//}
