0b34af7cccf306f1c7cfa1d88e704b7ece816996
FUNZHOU <130201517+Funzhou000@users.noreply.github.com>
2024-11-12 19:01:14 +0800

    Update 水仙花.cpp
    add some 注释

--- 翁恺c语言/水仙花.cpp
+++ 翁恺c语言/水仙花.cpp
@@ -1,4 +1,4 @@
-﻿#define  _CRT_SECURE_NO_WARNINGS
+#define  _CRT_SECURE_NO_WARNINGS
 #include <iostream> // 标准输入输出流
 #include <vector>   // 标准模板库中的向量
 #include <string>   // C++ 字符串类
@@ -19,12 +19,12 @@ int main() {
 	i = first;
 	while (i < first*10) {//遍历同一位数的所有数字eg：100-999,i为该位数最小的数字
 		int t = i;
-		int sum = 0;
+		int sum = 0;//需要在较外层定义变量以便可以在遍历中使用；
 		do {
-			int d = t % 10;
-			t /= 10;
-			int p = d;
-			int j = 1;
+			int d = t % 10;//分解循环到的数字的第一位-第二位。，。。。
+			t /= 10;//让数字的后面几位可以循环到
+			int p = d;//p为每位数的n次方
+			int j = 1;//让p的次幂正确
 			while (j < n) {
 				p *= d;//让一位数n次幂
 				j++;
